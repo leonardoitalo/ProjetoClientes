f053e7670c43bdc7d31aba3b973d58fc3312abcc
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-01-05 03:12:12 -0300

    Otimizando arquivo de gerar dados fakes

--- populate_script.py
+++ populate_script.py
@@ -1,11 +1,10 @@
 import os, django
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
 django.setup()
 
 from faker import Faker
 from validate_docbr import CPF
-import random
+from random import randrange, choice
 from clientes.models import Cliente
 
 def criando_pessoas(quantidade_de_pessoas):
@@ -14,12 +13,13 @@ def criando_pessoas(quantidade_de_pessoas):
     for _ in range(quantidade_de_pessoas):
         cpf = CPF()
         nome = fake.name()
-        email = '{}@{}'.format(nome.lower(),fake.free_email_domain())
-        email = email.replace(' ', '')
+        email = f"{nome.lower()}@{fake.free_email_domain()}"
+        email = email.replace(" ", "")
         cpf = cpf.generate()
-        rg = "{}{}{}{}".format(random.randrange(10, 99),random.randrange(100, 999),random.randrange(100, 999),random.randrange(0, 9) ) 
-        celular = "{} 9{}-{}".format(random.randrange(10, 21), random.randrange(4000, 9999), random.randrange(4000, 9999))
-        ativo = random.choice([True, False])
+        cpf = cpf.replace(".", "").replace("-", "")
+        rg = f"{randrange(10, 99)}{randrange(100, 999)}{randrange(100, 999)}{randrange(0, 9)}" 
+        celular = f"{randrange(10, 21)} 9{randrange(4000, 9999)}-{randrange(4000, 9999)}"
+        ativo = choice([True, False])
         p = Cliente(nome=nome, email=email, cpf=cpf, rg=rg, celular=celular, ativo=ativo)
         p.save()
 
