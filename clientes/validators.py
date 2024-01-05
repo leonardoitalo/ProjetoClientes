8387330ebc0b50ec502979f76d9253e1724c942a
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-01-05 01:02:04 -0300

    validando campo de número de celular

--- clientes/validators.py
+++ clientes/validators.py
@@ -1,3 +1,4 @@
+import re
 
 
 def cpf_valido(numero_do_cpf):
@@ -12,8 +13,12 @@ def rg_valido(numero_do_rg):
     return len(numero_do_rg) == 9
 
 
-def celular_valido(celular):
-    return len(celular) < 11
+def celular_valido(numero_celular):
+    """Verifica se o celular é valido (11 91345-1234)"""
+    modelo = "[0-9]{2} [0-9]{5}-[0-9]{4}"
+    resposta = re.findall(modelo, numero_celular)
+    return resposta
+
        
 
     
