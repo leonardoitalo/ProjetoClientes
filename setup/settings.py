f6e370fa4748e62606be61d9b35316bfa7b7f406
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-01-05 04:12:23 -0300

    Implementando ordenação de nomes de clientes

--- setup/settings.py
+++ setup/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'clientes'
+    'clientes',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
