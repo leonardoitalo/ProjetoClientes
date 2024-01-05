3573cd684bc7e67b3f6b9731babd768140de167a
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-01-05 00:41:05 -0300

    criando arquivo para validação

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 
 class ClienteSerializer(serializers.ModelSerializer):
@@ -7,26 +8,22 @@ class ClienteSerializer(serializers.ModelSerializer):
         model = Cliente
         fields = '__all__'
 
-    def validate_cpf(self, cpf):
-        if len(cpf) != 11:
-            raise serializers.ValidationError("O CPF deve ter 11 dígitos")
-        return cpf
+    def validate(self, data):
+        if not cpf_valido(data["cpf"]):
+            raise serializers.ValidationError({"cpf":"O CPF deve ter 11 dígitos"})
+        
+        if not nome_valido(data["nome"]):
+            raise serializers.ValidationError({"nome":"Não inclua números neste campo"})
+        
+        if not rg_valido(data["rg"]):
+            raise serializers.ValidationError({"rg":"O RG deve ter 9 dígitos"})
 
-
-    def validate_nome(self, nome):
-        if not nome.isalpha():
-            raise serializers.ValidationError("Não inclua números neste campo") 
-        return nome       
+        if not celular_valido(data["celular"]):
+            raise serializers.ValidationError({"celular":"O celular deve ter 11 dígitos"})  
+          
+        return data
     
 
-    def validate_rg(self, rg):
-        if len(rg) != 9:
-            raise serializers.ValidationError("O RG deve ter 9 dígitos")
-        return rg
+
     
 
-    def validate_celular(self, celular):
-        if len(celular) < 11:
-            raise serializers.ValidationError("O celular deve ter 11 dígitos")
-        return celular
-    
---
+++ clientes/validators.py
@@ -0,0 +1,21 @@
+
+
+def cpf_valido(numero_do_cpf):
+    return len(numero_do_cpf) == 11
+
+
+def nome_valido(nome): 
+    return nome.isalpha()
+        
+
+def rg_valido(numero_do_rg):
+    return len(numero_do_rg) == 9
+
+
+def celular_valido(celular):
+    return len(celular) < 11
+       
+
+    
+        
+    
